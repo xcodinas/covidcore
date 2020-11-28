cd8217762b2bf5678707bd31ad6c668f8712767e
Xavier Codinas <xavier19966@gmail.com>
2020-11-28 14:16:19 +0100

    Add tokenblacklist model

--- app/models.py
+++ app/models.py
@@ -60,5 +60,24 @@ class MedicalCenter(db.Model, TimestampsMixin):
     contact_methods = db.Column(db.String)
 
 
+class TokenBlacklist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    jti = db.Column(db.String, nullable=False)
+    token_type = db.Column(db.String, nullable=False)
+    user_identity = db.Column(db.String, nullable=False)
+    revoked = db.Column(db.Boolean, nullable=False)
+    expires = db.Column(db.DateTime, nullable=False)
+
+    def to_dict(self):
+        return {
+            'token_id': self.id,
+            'jti': self.jti,
+            'token_type': self.token_type,
+            'user_identity': self.user_identity,
+            'revoked': self.revoked,
+            'expires': self.expires
+        }
+
+
 User.register()
 MedicalCenter.register()
